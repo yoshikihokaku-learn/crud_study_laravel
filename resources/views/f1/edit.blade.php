33604260dce33de10ca7e6b8aeea5e44c2b5cbb5
yoshikihokaku-learn <yoshiki.formula.627@gmail.com>
2022-05-13 17:02:47 +0900

    編集画面変更

--- resources/views/f1/edit.blade.php
+++ resources/views/f1/edit.blade.php
@@ -4,22 +4,31 @@
         <h1>編集画面</h1>
         <form action="{{ url('f1/update', ['id'=>$driver->id]) }}" method="post">
             @csrf
-            <label for="">ドライバー名</label>
-            <input type="text" name="name" value="{{$driver->name}}">
-            <label for="">国籍</label>
-            <input type="text" name="country" value="{{$driver->country}}">
-            <label for="">カーNo.</label>
-            <input type="text" name="carNo" value="{{$driver->car_number}}">
-            <label for="">チーム</label>
-            <select name="team">
-                <option value="">~~ Select your team ~~</option>
-                @foreach ($teams as $t)
-                    <option value="{{$t->id}}"
-                        @if ($t->id === $driverTeam) selected @endif>
-                        {{$t->name}}</option>
-                @endforeach
-            </select>
-            <input type="submit" value="変更" class="btn">
+            <div class="mb-3">
+                <label for="name" class="form-label">ドライバー名</label>
+                <input type="text" name="name" value="{{$driver->name}}" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="country" class="form-label">国籍</label>
+                <input type="text" name="country" value="{{$driver->country}}" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="carNo" class="form-label">カーNo.</label>
+                <input type="text" name="carNo" value="{{$driver->car_number}}" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">チーム</label>
+                <select name="team" class="form-select">
+                    <option value="">~~ Select your team ~~</option>
+                    @foreach ($teams as $t)
+                        <option value="{{$t->id}}"
+                            @if ($t->id === $driverTeam) selected @endif>
+                            {{$t->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <input type="submit" value="変更" class="btn btn-success">
+            <a href="{{ route('drivers') }}" class="btn btn-secondary">戻る</a>
         </form>
     </div>
 </div>
